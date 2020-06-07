638201aec7495b82440698f6d4ec702936af311f
priyadharshan <priyadharshan91@gmail.com>
2020-06-08 04:49:22 +0530

    [sapient-123] build issue fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { Dashboard } from './dashboard'
-import './app.css';
+import './App.css';
 
 export function App() {
   return (
